a644d10994d827f239d0e61d72d0701d52056094
guillermo-st <guillermo_stancanelli@outlook.com>
2023-06-06 12:55:43 -0300

    Fix missing required binding and variable name

--- contracts/metrics/create.go
+++ contracts/metrics/create.go
@@ -1,6 +1,6 @@
 package metrics
 
 type CreateMetricRequest struct {
-	MetricType string
+	MetricType string `binding:"required"`
 	SubType    string
 }
--- contracts/metrics/validate.go
+++ contracts/metrics/validate.go
@@ -2,17 +2,18 @@ package metrics
 
 import "errors"
 
-var validTypeSubTypes = map[string]struct{}{
+var validMetricTypes = map[string]struct{}{
 	"register":         {},
 	"login":            {},
 	"blocked":          {},
 	"password_recover": {},
 	"location":         {},
 	"new_training":     {},
+	"training_tagged":  {},
 }
 
 func ValidateMetricTypes(metricType string, subType string) error {
-	if _, ok := validTypeSubTypes[metricType]; !ok {
+	if _, ok := validMetricTypes[metricType]; !ok {
 		return errors.New("Invalid metric type")
 	}
 	if (metricType == "register" || metricType == "login") && (subType != "mail" && subType != "federated_entity") {
